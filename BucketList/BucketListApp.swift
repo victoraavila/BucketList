8a4897db980b21ade28718a25a103dcd79b9fed5
Víctor Ávila <victoraavila2000@gmail.com>
2024-07-23 00:54:44 -0300

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Víctor Ávila on 23/07/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
